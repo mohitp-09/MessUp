379d0d809372114b58c39bc0f8c53915e959f282
mohitpatidardev <mohitpatidar2186@gmail.com>
2025-05-23 15:03:32 +0530

    changes in frontend

--- Proj-chat-website(frontend)/vite.config.js
+++ Proj-chat-website(frontend)/vite.config.js
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'robots.txt'],
+      manifest: {
+        name: 'MessUp',
+        short_name: 'MessUp',
+        description: 'Chat beautifully.',
+        theme_color: '#0f172a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ],
+  server: {
+    host: true
+  }
+});
